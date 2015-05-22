1345e9bcbd727566edc36116d519ede4a57336fd
Alex Rodriguez <alexrodriguez@Alexs-MacBook-Pro-3.local>
2015-05-22 00:35:19 -0400

    Frivolous additions

--- src/main/scala/Sudoku.scala
+++ src/main/scala/Sudoku.scala
@@ -1,5 +1,26 @@
 object Sudoku {
   def main(args: Array[String]) = {
+    val state = new Sudoku
+    println(state.toString())
     println("Eventually this will do stuff with Sudoku Puzzles.")
+  }
+}
+
+class Sudoku {
+
+  val puzzle = Array(
+    0,0,6,0,0,7,3,0,0,
+    0,1,8,0,0,9,0,5,0,
+    5,0,0,0,0,0,0,6,4,
+    9,2,0,0,8,0,0,0,0,
+    0,0,0,7,6,3,0,0,0,
+    0,0,0,0,9,0,0,7,5,
+    6,3,0,0,0,0,0,0,8,
+    0,9,0,3,0,0,5,2,0,
+    0,0,2,4,0,0,6,0,0
+    )
+
+  override def toString(): String = {
+    "Betcha wish this was a sudoku"
   }
 }
